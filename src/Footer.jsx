4e86e3156b47c5832897de358876c39ad3b30762
Linh <linhtrant433@gmail.com>
2024-10-26 15:56:12 -0500

    frame of the website

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import './assets/Footer.css'
+function Footer () {
+    return (
+        <footer className="footer">
+            <p>&copy; {new Date().getFullYear()} BooWay</p>
+        </footer>
+    );
+}       
+export default Footer; 
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import './assets/Header.css'
+function Header () {
+    return (
+        <header>
+            <h1>BooWay</h1>
+            <nav>
+                <ul>
+                      
+                </ul>
+            </nav>
+        </header>
+        
+    )
+} 
+
+export default Header;
